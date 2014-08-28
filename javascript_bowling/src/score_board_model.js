1159a7953055a81f0be941ccedc7c7fe07198cb0
Ziad Abdo <mr.ziad.abdo@gmail.com>
2014-08-28 12:19:40 -0700

    *implemented capability to add rows for multiple players 	*renamed some variables to comply with proper naming 		conventions

--- javascript_bowling/src/score_board_model.js
+++ javascript_bowling/src/score_board_model.js
@@ -1,17 +1,26 @@
 window.addEventListener('load', function(){
-	newBoard = new ScoreBoard()
-	newBoard.setUpFirstRow()	
+	var newBoard = new ScoreBoard()
+	newBoard.setUpFirstRow()
+	var numberOfPlayers = prompt("Enter the number of players", "1")
+	newBoard.setUpColumns(numberOfPlayers)
 })
 
 function ScoreBoard(){
-	this.player_row = document.querySelector('.player-row')
-	this.score_frame = document.querySelector('.score-frame')
+	this.playerRow = document.querySelector('.player-row')
+	this.scoreFrame = document.querySelector('.score-frame')
+	this.scoreBoard = document.querySelector('.score-board')
 }
 
 ScoreBoard.prototype = {
 	setUpFirstRow: function(){
 		for(var i=1; i<10; i++){
-			this.player_row.appendChild(this.score_frame.cloneNode(true,true))
+			this.playerRow.appendChild(this.scoreFrame.cloneNode(true,true))
+		}
+	},
+	setUpColumns: function(numberOfPlayers){
+		this.playerRow = document.querySelector('.player-row')
+		for(var i=1; i<numberOfPlayers; i++){
+			this.scoreBoard.appendChild(this.playerRow.cloneNode(true,true))
 		}
 	}
 }
