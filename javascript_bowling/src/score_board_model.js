54ee0f8786b0492bfeff5de2ca52ed91d4eaca99
Ziad Abdo <mr.ziad.abdo@gmail.com>
2014-08-28 16:45:01 -0700

    *I moved some functionality from the window listener 		into the model. 	*added functionality that will ask for names based on the 		number of players 	*the names are displayed next to their respective rows.

--- javascript_bowling/src/score_board_model.js
+++ javascript_bowling/src/score_board_model.js
@@ -1,14 +1,15 @@
 window.addEventListener('load', function(){
 	var newBoard = new ScoreBoard()
 	newBoard.setUpFirstRow()
-	var numberOfPlayers = prompt("Enter the number of players", "1")
-	newBoard.setUpColumns(numberOfPlayers)
+	newBoard.playerAmountPrompt()	
+	newBoard.playerNamesPrompt()
 })
 
 function ScoreBoard(){
 	this.playerRow = document.querySelector('.player-row')
 	this.scoreFrame = document.querySelector('.score-frame')
 	this.scoreBoard = document.querySelector('.score-board')
+	this.numberOfPlayers = 0
 }
 
 ScoreBoard.prototype = {
@@ -22,5 +23,22 @@ ScoreBoard.prototype = {
 		for(var i=1; i<numberOfPlayers; i++){
 			this.scoreBoard.appendChild(this.playerRow.cloneNode(true,true))
 		}
+	}, 
+	playerAmountPrompt: function(){
+		this.numberOfPlayers = prompt("Enter the number of players", "1")
+		this.setUpColumns(this.numberOfPlayers)	
+	},
+	playerNamesPrompt: function(){
+		var playersNames = []
+		for(var i=0; i<this.numberOfPlayers; i++){
+			playersNames.push( prompt( "Enter a name for player" + ( i+1 ), "Name") )
+		}
+		this.setUpPlayerNames(playersNames)
+	},
+	setUpPlayerNames: function(playersNames){
+		for(var i=0; i<playersNames.length; i++){
+			document.querySelectorAll('.player-name')[i].innerHTML = playersNames[i]
+		}
 	}
 }
+
